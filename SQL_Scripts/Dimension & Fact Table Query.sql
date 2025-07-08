13372ee8e82fd5e8113a6099a8843d59d47fb187
rawoofcryptos <srsrrawoof007@gmail.com>
2025-07-08 14:54:27 +0100

    Update Dimension & Fact Table Query.sql
    Updated Dimenstion Table and Fact Table Query

--- SQL_Scripts/Dimension & Fact Table Query.sql
+++ SQL_Scripts/Dimension & Fact Table Query.sql
@@ -10,3 +10,45 @@ CREATE TABLE DimDate (
 
 Select * from DimDate where Year=2025
 
+CREATE TABLE DimAirline (
+    AirlineKey INT PRIMARY KEY IDENTITY(1,1), -- Surrogate Key
+    AirlineCode VARCHAR(5) NOT NULL,         -- Natural Key
+    AirlineName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE DimAirport (
+    AirportKey INT PRIMARY KEY IDENTITY(1,1), -- Surrogate Key
+    AirportCode VARCHAR(3) NOT NULL,         -- Natural Key
+    AirportName VARCHAR(70) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    State VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE FactFlightSummary (
+    DateKey VARCHAR(15) NOT NULL,
+    AirlineKey INT NOT NULL,
+    AirportKey INT NOT NULL,
+    ArrivalFlights INT,
+    DelayedFlightsCount INT,
+    CarrierDelayCount FLOAT,
+    WeatherDelayCount FLOAT,
+    NASDelayCount FLOAT,
+    SecurityDelayCount FLOAT,
+    LateAircraftDelayCount FLOAT,
+    CancelledFlights INT,
+    DivertedFlights INT,
+    TotalArrivalDelayMinutes INT,
+    CarrierDelayMinutes INT,
+    WeatherDelayMinutes INT,
+    NASDelayMinutes INT,
+    SecurityDelayMinutes INT,
+    LateAircraftDelayMinutes INT,
+    
+    -- Define the Composite Primary Key
+    PRIMARY KEY (DateKey, AirlineKey, AirportKey),
+    
+    -- Define the Foreign Key Constraints
+    FOREIGN KEY (DateKey) REFERENCES DimDate(DateKey),
+    FOREIGN KEY (AirlineKey) REFERENCES DimAirline(AirlineKey),
+    FOREIGN KEY (AirportKey) REFERENCES DimAirport(AirportKey)
+);
